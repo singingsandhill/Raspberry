75ffb88be0e452b18d7af69c021e6c80239dc38b
Ubuntu <ubuntu@ip-172-31-45-196.ap-northeast-2.compute.internal>
2025-11-07 03:25:40 +0000

    Test:Disable @SpringBootTest in all services to accelerate CI test job

--- user/src/test/java/softbank/hackathon/user/UserApplicationTests.java
+++ user/src/test/java/softbank/hackathon/user/UserApplicationTests.java
@@ -1,9 +1,9 @@
 package softbank.hackathon.user;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class UserApplicationTests {
 
 	@Test
