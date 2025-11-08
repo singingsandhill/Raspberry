2c1d70efab2d93a149806d6165e4071c011165d3
logan <2220110150@office.kopo.ac.kr>
2025-11-09 00:03:54 +0900

    fix: frontend controller test2

--- fe/src/main/java/softbank/hackathon/fe/presentation/controller/FeController.java
+++ fe/src/main/java/softbank/hackathon/fe/presentation/controller/FeController.java
@@ -2,6 +2,8 @@ package softbank.hackathon.fe.presentation.controller;
 
 import org.springframework.stereotype.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -13,5 +15,5 @@ public class FeController {
         log.info("루트 페이지 요청 받음");
         return "index"; // templates/index.html을 반환
     }
-    
+
 }
